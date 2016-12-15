20192df801ace6630caf6e8fb24ad2c1f9ee3af2
Carlos Azaustre <cazaustre@gmail.com>
2016-12-15 15:45:09 +0100

    add webpack config files

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  extensions: ['', '.js'],
+  devTool: 'cheap-module-eval-source-map',
+  noInfo: false,
+  target: 'web',
+
+  entry: [
+    'webpack-dev-server/client',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style', 'css?sourceMap'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loaders: ['file'] },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    inline: true,
+    contentBase: './src'
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  extensions: ['', '.js'],
+  devTool: 'source-map',
+  noInfo: false,
+  target: 'web',
+
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap') },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loaders: ['file'] },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'src', 'index.html') }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    inline: true,
+    contentBase: './dist'
+  }
+};
