2cde306f825189934a83c2f705fb4809a155d08e
Carlos Azaustre <cazaustre@gmail.com>
2016-12-15 17:56:05 +0100

    rename MainContainer to TodoContainer

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App'
-import MainContainer from './components/main/MainContainer'
+import TodoContainer from './components/todo/TodoContainer'
 import SectionContainer from './components/section/SectionContainer'
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={MainContainer} />
+    <IndexRoute component={TodoContainer} />
     <Route path="/section" component={SectionContainer} />
   </Route>
 );
