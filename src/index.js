adde2ff7e8df8574d41dc0131c90b2be555fad56
Carlos Azaustre <cazaustre@gmail.com>
2016-12-15 18:37:33 +0100

    configure store with react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-
+import { syncHistoryWithStore } from 'react-router-redux';
 import 'materialize-css/dist/css/materialize.css';
+
+import configureStore from './store/configureStore';
 import routes from './routes';
 
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
