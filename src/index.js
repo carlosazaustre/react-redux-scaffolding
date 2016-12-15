09b16f554c72a8fb16f4c39415c5643c0f207616
Carlos Azaustre <cazaustre@gmail.com>
2016-12-15 16:52:47 +0100

    add babel config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola Mundo!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
